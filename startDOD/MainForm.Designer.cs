21a8dded06d3f4087c360cee5430d39d6bbf5d26
K64t34 <k64t34@gmail.com>
2019-01-06 01:20:38 +0300

    05012019

---
+++ startDOD/MainForm.Designer.cs
@@ -0,0 +1,122 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Andrew
+ * Date: 05.01.2019
+ * Time: 19:33
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+namespace startDOD
+{
+	partial class MainForm
+	{
+		/// <summary>
+		/// Designer variable used to keep track of non-visual components.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.TextBox textBox_Console;		
+		private System.Windows.Forms.Button button2;
+		private System.Windows.Forms.Button button_install2;
+		
+		
+		/// <summary>
+		/// Disposes resources used by the form.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing) {
+				if (components != null) {
+					components.Dispose();
+				}
+			}
+			base.Dispose(disposing);
+		}
+		
+		/// <summary>
+		/// This method is required for Windows Forms designer support.
+		/// Do not change the method contents inside the source code editor. The Forms designer might
+		/// not be able to load this method if it was changed manually.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.button1 = new System.Windows.Forms.Button();
+			this.textBox_Console = new System.Windows.Forms.TextBox();
+			this.button2 = new System.Windows.Forms.Button();
+			this.button_install2 = new System.Windows.Forms.Button();
+			this.SuspendLayout();
+			// 
+			// button1
+			// 
+			this.button1.AutoSize = true;
+			this.button1.BackColor = System.Drawing.Color.Transparent;
+			this.button1.FlatAppearance.BorderSize = 0;
+			this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+			this.button1.Location = new System.Drawing.Point(28, 393);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(75, 31);
+			this.button1.TabIndex = 0;
+			this.button1.Text = "Выход";
+			this.button1.UseVisualStyleBackColor = false;
+			this.button1.Click += new System.EventHandler(this.Button1Click);
+			// 
+			// textBox_Console
+			// 
+			this.textBox_Console.BackColor = System.Drawing.SystemColors.ControlDark;
+			this.textBox_Console.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+			this.textBox_Console.Location = new System.Drawing.Point(143, 29);
+			this.textBox_Console.Multiline = true;
+			this.textBox_Console.Name = "textBox_Console";
+			this.textBox_Console.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+			this.textBox_Console.Size = new System.Drawing.Size(650, 395);
+			this.textBox_Console.TabIndex = 1;
+			// 
+			// button2
+			// 
+			this.button2.Location = new System.Drawing.Point(42, 232);
+			this.button2.Name = "button2";
+			this.button2.Size = new System.Drawing.Size(75, 23);
+			this.button2.TabIndex = 2;
+			this.button2.Text = "Refresh";
+			this.button2.UseVisualStyleBackColor = true;
+			this.button2.Click += new System.EventHandler(this.Button2Click);
+			// 
+			// button_install2
+			// 
+			this.button_install2.AutoSize = true;
+			this.button_install2.BackColor = System.Drawing.Color.Transparent;
+			this.button_install2.FlatAppearance.BorderSize = 0;
+			this.button_install2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+			this.button_install2.Location = new System.Drawing.Point(28, 25);
+			this.button_install2.Name = "button_install2";
+			this.button_install2.Size = new System.Drawing.Size(82, 31);
+			this.button_install2.TabIndex = 0;
+			this.button_install2.Text = "Install";
+			this.button_install2.UseVisualStyleBackColor = false;
+			this.button_install2.Click += new System.EventHandler(this.button_installClick);
+			// 
+			// MainForm
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 19F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.BackColor = System.Drawing.Color.Gray;
+			this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+			this.ClientSize = new System.Drawing.Size(805, 467);
+			this.Controls.Add(this.button2);
+			this.Controls.Add(this.textBox_Console);
+			this.Controls.Add(this.button_install2);
+			this.Controls.Add(this.button1);
+			this.Font = new System.Drawing.Font("Consolas", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+			this.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+			this.Margin = new System.Windows.Forms.Padding(4);
+			this.Name = "MainForm";
+			this.Text = "День победы";
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+		
+	}
+}
---
+++ startDOD/MainForm.cs
@@ -0,0 +1,132 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Andrew
+ * Date: 05.01.2019
+ * Time: 19:33
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+//using System.Collections.Generic;
+//using System.Drawing;
+using System.Windows.Forms;
+using System.Diagnostics;
+//using System.IO;
+//using System.Net;
+using System.Threading;
+//using System.Reflection;
+using System.Text;
+
+namespace startDOD
+{
+	/// <summary>
+	/// Description of MainForm.
+	/// </summary>	
+	public partial class MainForm : Form
+	{
+		
+		private Process cmd = null;		
+		private Thread thread = null;		
+		delegate  void delegateAppendText (string text);
+		delegateAppendText AppendText;
+		int numOutputLines=0;
+		public MainForm()
+		{			
+		
+			//
+			// The InitializeComponent() call is required for Windows Forms designer support.
+			//
+			InitializeComponent();
+			
+			//
+			// TODO: Add constructor code after the InitializeComponent() call.
+			//		
+		}
+		void Button1Click(object sender, EventArgs e)
+		{
+			this.Hide();
+			if (thread != null)
+			{
+				if (cmd !=null) //if (cmd.StartInfo.RedirectStandardOutput)//if (!cmd.HasExited)
+				{
+				try
+					{
+						cmd.Kill();
+					}
+					finally{}
+				}
+				
+				thread.Abort();
+			}
+			this.Close();
+		}
+		void button_installClick(object sender, EventArgs e)
+		{
+			button_install2.Enabled=false;
+			textBox_Console.AppendText("Start thread\r\n");
+			cmd = new Process();
+			cmd.StartInfo.RedirectStandardOutput = true;
+			cmd.StartInfo.RedirectStandardError = true;
+			cmd.StartInfo.CreateNoWindow = true;
+			//cmd.StartInfo.WorkingDirectory=srcds_folder;			
+			//"C:\Program Files\7-Zip\7z.exe" a -bb1 -uz2 E:\tmp\test-startDOD "D:\Program Files (x86)\Steam\steamapps\common\Day of Defeat Source\dod"
+			cmd.StartInfo.FileName = @"C:\Program Files\7-Zip\7z.exe";			
+			cmd.StartInfo.UseShellExecute=false;	//https://msdn.microsoft.com/ru-ru/library/system.diagnostics.processstartinfo.workingdirectory(v=vs.110).aspx			
+			cmd.StartInfo.Arguments=@"a -bb1 -uz2 E:\tmp\test-startDOD";
+			cmd.StartInfo.Arguments+=@" ""D:\Program Files (x86)\Steam\steamapps\common\Day of Defeat Source\dod""";
+			cmd.StartInfo.Arguments+=@" 2>&1";
+			//cmd.StartInfo.Arguments+=@"\materials""";
+			cmd.StartInfo.UseShellExecute = false;
+			cmd.OutputDataReceived += new DataReceivedEventHandler(OutputHandler);
+			cmd.ErrorDataReceived  += new DataReceivedEventHandler(OutputHandler);
+			
+			thread = new Thread(this.RunCMD);
+			thread.Name="RunCMD";			
+			AppendText= new delegateAppendText(this.textBox_Console.AppendText);
+			thread.Start();			
+			textBox_Console.AppendText("Finish method\r\n");
+			
+		}
+		 private  void RunCMD()
+		 	// создание трита внутри формы https://docs.microsoft.com/ru-ru/dotnet/framework/winforms/controls/how-to-make-thread-safe-calls-to-windows-forms-controls
+		{			
+			
+		 	//origin  this.Invoke(AppendText, new object[] { "sadf" });
+		 	this.Invoke(AppendText,"Start cmd\r\n");		 	
+		 	cmd.Start();
+		 	cmd.BeginOutputReadLine();			
+			cmd.WaitForExit();
+			this.Invoke(AppendText,"finish cmd\r\n");
+			
+			
+			//cmd.StartInfo.RedirectStandardOutput = false;			
+			//this.button_install.Enabled=true;
+			//cmd.Dispose();
+			cmd=null;
+		}
+		private  async void OutputHandler(object sendingProcess, 
+            DataReceivedEventArgs outLine)
+        {
+            numOutputLines++;
+		 	// Collect the sort command output.
+		 	//String result;
+		 	//result = await cmd.StandardOutput.ReadLineAsync();
+            if (!String.IsNullOrEmpty(outLine.Data))
+            	{
+            	//StringBuilder sortOutput = new StringBuilder("");
+            	//sortOutput.Append(outLine.Data);
+            	//this.Invoke(AppendText,sortOutput.ToString());
+            	this.Invoke(AppendText,Environment.NewLine +outLine.Data.ToString());
+            /*// Add the text to the collected output.
+                sortOutput.Append(Environment.NewLine + 
+                    "[" + numOutputLines.ToString() + "] - " + outLine.Data);*/
+            }
+        }
+		
+		void Button2Click(object sender, EventArgs e)
+		{
+			this.textBox_Console.AppendText(numOutputLines.ToString());
+			//this.textBox_Console.Refresh();
+		} 
+	}
+}
